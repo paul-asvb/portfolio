68894e3040cc4850d39f79032c2c497186ede907
Paul Mayer <paul@paul-mayer.eu>
2023-11-12 00:10:06 +0100

    work on projects

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import content from '@originjs/vite-plugin-content'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), content()],
 })
