eb6e85df647c279218d3ab1893023994005829b3
Paul Mayer <paul@paul-mayer.eu>
2023-11-13 17:07:40 +0100

    update content

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,112 @@
-import React from "react";
 import content from "./projects.toml";
 
 const projects = content.projects;
 
 const App = () => (
-  <div class="container mx-auto py-8 flex flex-col w-2/3 print:w-full">
-    <div class="col-span-4 sm:col-span-3 bg-white ">
-      <div class="flex flex-col items-center">
+  <div className="bg-paul-100 container mx-auto py-8 flex flex-col w-2/3 print:w-full">
+    <div className="col-span-4 sm:col-span-3">
+      <div className="flex flex-col items-center">
         <img
           src="/paul_mayer.png"
-          class="w-32 h-32 object-cover bg-gray-300 rounded-full mb-4 shrink-0"
+          className="w-32 h-32 object-cover bg-gray-300 rounded-full mb-4 shrink-0"
         ></img>
-        <h1 class="text-xl font-bold">Paul Mayer</h1>
-        <p class="text-gray-600">Software Developer</p>1
+        <h1 className="text-xl font-bold text-paul-900">Paul Mayer</h1>
+        <p className="text-gray-600">Software Developer</p>
+        <div>
+          <a
+            className="m-1 text-paul-700 underline"
+            href="https://www.linkedin.com/in/paul-mayer-0b9457ba/"
+          >
+            linkedin
+          </a>
+          <a
+            className="m-1 text-paul-700 underline"
+            href="https://github.com/paul-asvb"
+          >
+            github
+          </a>
+        </div>
       </div>
     </div>
-    <h2 class="text-xl font-bold mb-4">About Me</h2>
-    <p class="text-gray-700">
-      I am a passionate software developer and entrepreneur with a keen interest in crafting efficient and innovative solutions to complex problems.
-      My journey in the realm of coding began as a young boy with writing programs for my own entertainment.
-      Besides my interest in computers i always had a strong pull towards entrepreneurship, i started multiple companies with variying levels of success.
-      My latest company deepc, based here in munich, has grown to around 60 people with over 2 Mio. € in ARR.
-      My commitment to staying abreast of the latest industry trends and technologies ensures that my work is not only cutting-edge
-      but also aligns seamlessly with the ever-evolving landscape of software development and tech management.
-      In addition to my technical skills, I understand the importance of effective communication in translating complex technical concepts into layman's terms.
-      Articulating ideas and concepts clearly and concisely, fostering a collaborative and inclusive environment and motivating my peers and students will help
-      me to not only transfer knowledge, but spark the entrepreneurial spirit in a dynamic environments where innovation brings a sense of joy and accomplishment.
-      I enjoy working with cross-functional teams, drawing on diverse perspectives to create products that not only meets but exceeds expectations.
-      Seeking for opportunities to learn, grow, and contribute my skills in a way that make a meaningful impact is my goal for every project i start.
+    <h2 className="text-2xl font-bold mb-4 text-paul-900">About Me</h2>
+    <p>
+      I am a passionate software developer and entrepreneur with a keen interest
+      in crafting efficient and innovative solutions to complex problems. My
+      journey in the realm of coding began as a young boy with writing programs
+      for my own entertainment. Besides my interest in computers i always had a
+      strong pull towards entrepreneurship, i started multiple companies with
+      variying levels of success. My latest company deepc, based here in munich,
+      has grown to around 60 people and offices in the UK, US and Australia.
+    </p>
+    <p>
+      My commitment to staying abreast of the latest industry trends and
+      technologies ensures that my work is not only cutting-edge but also aligns
+      seamlessly with the ever-evolving landscape of software development and
+      tech management. In addition to my technical skills, I understand the
+      importance of effective communication in translating complex technical
+      concepts into layman's terms.
+    </p>
+    <p>
+      Articulating ideas and concepts clearly and concisely, fostering a
+      collaborative and inclusive environment and motivating my peers and
+      students will help me to not only transfer knowledge, but spark the
+      entrepreneurial spirit. We all want work in dynamic environments where
+      innovation brings a sense of joy and accomplishment. I enjoy working with
+      cross-functional teams, drawing on diverse perspectives to create products and services
+      that not only meets but exceeds expectations. I want to learn, grow, and
+      contribute my skills in a way that make a meaningful impact.
     </p>
 
-    <h2 class="text-xl font-bold mt-6 mb-4">Experience</h2>
-    {
-      projects.map((p) => (
-        <div class="mb-6">
-          <div class="flex justify-between">
-            <span class="text-gray-600 font-bold">{p.partner} - {p.position}</span>
-            <p>
-              <span class="text-gray-600">{p.from} - {p.to}</span>
-            </p>
-          </div>
-          <p class="mt-2">
-            {p.description}
+    <h2 className="text-2xl font-bold mt-6 mb-4">Experience</h2>
+    {projects.map((p) => (
+      <div
+        key={p.partner}
+        className="mt-4 pt-4 mb-6 border-double border-paul-950 border-t"
+      >
+        <div className="flex justify-between">
+          <span className="font-bold">{p.partner}</span>
+          <p>
+            <span className="">
+              {p.from} - {p.to}
+            </span>
           </p>
         </div>
-      ))
-    }
+        <p className="text-gray-600 ">{p.position}</p>
+        <p className="mt-2">{p.description}</p>
+        <div className="mt-4 justify-left select-none flex">
+          {p.tech.map((t) => (
+            <div
+              key={t}
+              className="py-1 px-3 mr-2 no-underline rounded-full bg-paul-400 text-paul-800 text-sm "
+            >
+              {t}
+            </div>
+          ))}
+        </div>
+      </div>
+    ))}
 
+    <h2 className="text-2xl font-bold mt-6 mb-4">Education</h2>
+    <div className="mt-4 pt-4 mb-6 border-double border-paul-950 border-t">
+      <div className="flex justify-between">
+        <span className="font-bold">Akademie U5</span>
+        <p>
+          <span className="text-gray-600">2010-2013</span>
+        </p>
+      </div>
+      <p className="mt-2">Kommunikationsdesign</p>
+    </div>
+    <div className="mt-4 pt-4 mb-6 border-double border-paul-950 border-t">
+      <div className="flex justify-between">
+        <span className="font-bold">LMU München</span>
+        <p>
+          <span className="text-gray-600">2007-2010</span>
+        </p>
+      </div>
+      <p className="mt-2">
+        Volkswirtschaft mit Nebenfach Politik und Soziologie
+      </p>
+    </div>
   </div>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paul: {
+          DEFAULT: "#77CF7E",
+          50: "#E1F3DD",
+          100: "#D6EFD2",
+          200: "#BFE7BB",
+          300: "#A6DFA4",
+          400: "#8ED790",
+          500: "#77CF7E",
+          600: "#46BE57",
+          700: "#328F44",
+          800: "#215E30",
+          900: "#102D19",
+          950: "#07150C",
+        },
+      },
+    },
   },
   plugins: [],
 };
